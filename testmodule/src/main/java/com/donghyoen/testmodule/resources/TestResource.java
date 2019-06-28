50b6c15b782efb124767256384dc5ed9005e9c24
민동현 <mindonghyeon890@gmail.com>
2019-06-28 17:21:01 +0900

    add tutorial code

---
+++ testmodule/src/main/java/com/donghyoen/testmodule/resources/TestResource.java
@@ -0,0 +1,17 @@
+package com.donghyoen.testmodule.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestResource {
+
+    @RequestMapping("/")
+    public String testModule() throws InterruptedException {
+
+        //delay
+        Thread.sleep(3000);
+
+        return "this is TestModule";
+    }
+}
